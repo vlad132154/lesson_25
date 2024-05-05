5c1cddba96f01cb23c133a1da1045a107c600b1e
temuch <ap17022007@mail.ru>
2024-05-05 18:13:43 +0300

    1

--- qwerty.py
+++ qwerty.py
@@ -5,14 +5,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    main_data = {
+        'Город': 'Новочебоксарск',
+        'Район': 'Обычный',
+        'Компания': 'Iserv'
+    }
+    bam = {
+        'Имя': 'Влад',
+        'Возраст': '16',
+        'Специализация': 'учусь'
+    }
+    return render_template('index.html', main_data=main_data, **bam)
 
 
 @app.route("/contacts/")
 def contacts():
-    developer_name = "Elon Mask"
+    developer_name = "Влад"
     telephone = "88005553535"
     return render_template('c.html', name=developer_name, tel=telephone)
+
+
+@app.route("/results/")
+def results():
+    data = ['Python', 'js', 'Golang', 'C#', 'C++', 'Rust', 'C']
+    return render_template('results.html', data=data)
 
 
 if __name__ == "__main__":
